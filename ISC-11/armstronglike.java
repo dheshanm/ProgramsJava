90188813088d2e23d317b420a4bc0b6b79cb4fba
Dheshan M <dheshan.mail@gmail.com>
2016-02-16 19:42:53 +0530

    Update armstronglike.java

--- ISC-11/armstronglike.java
+++ ISC-11/armstronglike.java
@@ -1,12 +1,17 @@
 import java.io.*;
-class armstronglike
-{
+import java.util.Scanner;
+
+/**
+ * Created by Dheshan M (@D-codex) on 11/21/2015.
+ */
+public class armstrongLike {
     public static void main(String ar[])throws IOException
     {
         int n,n1,n2,c=0,d=0,e,f=0;
-        DataInputStream in=new DataInputStream(System.in);
-        System.out.println("Enter a number.The number of digits in the entered number should be divisile by 3");
-        n=Integer.parseInt(in.readLine());
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter a number such that the number of digits in the entered number should be divisible by 3 " +
+                "\nEnter the Number    :");
+        n=in.nextInt();
         n1=n;
         n2=n;
         while(n>0)
@@ -14,18 +19,20 @@ class armstronglike
             c=c+1;
             n=n/10;
         }
-        if(c%3==0)
-         d=c/3;
-        while(n1>0)
-        {
-            e=(int)(n1%(Math.pow(10,d)));
-            f=f+(e*e*e);
-            n1=(int)(n1/(Math.pow(10,d)));
+        if(c%3==0) {
+            d = c / 3;
+            while (n1 > 0) {
+                e = (int) (n1 % (Math.pow(10, d)));
+                f = f + (e * e * e);
+                n1 = (int) (n1 / (Math.pow(10, d)));
+            }
+            System.out.println();
+            if (f == n2)
+                System.out.println("The number is an Armstronglike number");
+            else
+                System.out.println("The number is not an Armstornglike number");
         }
-        System.out.println();
-        if(f==n2)
-         System.out.println("The number is an Armstronglike number");
-        else
-         System.out.println("The number is not an Armstornglike number");
+        if(c%3!=0)
+        System.out.println("The number doesn't meet the mentioned condition");
     }
 }
